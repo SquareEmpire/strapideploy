19c8dc0776733e4e85a512af08e15d9e6dd6ed65
SquareEmpire <rk@Ryans-MacBook-Pro.local>
2023-02-08 14:04:46 -0500

    production

---
+++ config/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+    // ...
+    seo: {
+        enabled: true,
+    },
+    ckeditor: {
+        enabled: true,
+    },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
