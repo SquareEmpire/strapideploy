bd275c3cdd0d131512038f8ba7197c8ae75afa88
SquareEmpire <rk@Ryans-MacBook-Pro.local>
2023-02-14 13:42:56 -0500

    vday23

--- src/admin/app.js
+++ src/admin/app.js
@@ -18,7 +18,15 @@ export default {
         buttonPrimary600: myPrimaryColor,
         primary600: myPrimaryColor,
       },
-    }
+    },
+    translations: {
+      en: {
+        'app.components.LeftMenu.navbrand.title': 'Tech Square',
+        'app.components.LeftMenu.navbrand.title': 'Tech Square',
+        'Auth.form.welcome.title': 'Welcome to Tech Square!',
+        'Auth.form.welcome.subtitle': 'Log in to your App',
+      },
+    },
   },
   bootstrap(app) {
     console.log(app);
