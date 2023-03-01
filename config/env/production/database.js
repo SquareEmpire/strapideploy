00471ef7b0301e4a4a42199d41ce015450cac344
SquareEmpire <rk@Ryans-MacBook-Pro.local>
2023-03-01 13:48:21 -0500

    Production#1

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,20 @@
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT'),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
+            host,
+            port,
+            database,
+            user,
+            password,
             ssl: {
-                ca: env('DATABASE_CA'),
+                ca: env('DATABASE_CA')
             },
             debug: false,
 
